8d126dd4fa792aa5d4d2da7665daa5a569b60e38
cookie0_o <81589649+cookie0o@users.noreply.github.com>
2021-11-13 19:27:27 +0100

    NEW: basic error handler!
    you´ll get a message when a error occures instead of the programm closing!

--- crypter.py
+++ crypter.py
@@ -11,32 +11,34 @@ ver = ("v1.2")
 
 
 def main():
-    import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW(f"crypter [{ver}]")
-    #logo-version
-    print (f"""{Back.BLACK}{Fore.LIGHTCYAN_EX}\
-     .o88b. d8888b. db    db d8888b. d888888b d88888b d8888b. 
-    d8P  Y8 88  `8D `8b  d8' 88  `8D `~~88~~' 88'     88  `8D 
-    8P      88oobY'  `8bd8'  88oodD'    88    88ooooo 88oobY' 
-    8b      88`8b      88    88~~~      88    88~~~~~ 88`8b   
-    Y8b  d8 88 `88.    88    88         88    88.     88 `88. 
-     `Y88P' 88   YD    YP    88         YP    Y88888P 88   YD """)
-    print (f"{Back.BLACK}{Fore.LIGHTRED_EX}{ver}                          [made by; cookie-_-#2131 on dc]")
-    print (" ")
+    try:
+        import ctypes
+        ctypes.windll.kernel32.SetConsoleTitleW(f"crypter [{ver}]")
+        #logo-version
+        print (f"""{Back.BLACK}{Fore.LIGHTCYAN_EX}\
+        .o88b. d8888b. db    db d8888b. d888888b d88888b d8888b. 
+        d8P  Y8 88  `8D `8b  d8' 88  `8D `~~88~~' 88'     88  `8D 
+        8P      88oobY'  `8bd8'  88oodD'    88    88ooooo 88oobY' 
+        8b      88`8b      88    88~~~      88    88~~~~~ 88`8b   
+        Y8b  d8 88 `88.    88    88         88    88.     88 `88. 
+        `Y88P' 88   YD    YP    88         YP    Y88888P 88   YD """)
+        print (f"{Back.BLACK}{Fore.LIGHTRED_EX}{ver}                          [made by; cookie-_-#2131 on dc]")
+        print (" ")
 
-    #ask input
-    print (f"{Back.BLACK}{Fore.LIGHTMAGENTA_EX}do you want to decrypt[1] / encrypt[2] your file/folder ?")
-    #get input
-    en_decrypt = input("")
-    #process input
-    if en_decrypt == "2":
-        print(" ")
-        #encrypt-file-folder
-        encrypt_func()
+        #ask input
+        print (f"{Back.BLACK}{Fore.LIGHTMAGENTA_EX}do you want to decrypt[1] / encrypt[2] your file/folder ?")
+        #get input
+        en_decrypt = input("")
+        #process input
+        if en_decrypt == "2":
+            print(" ")
+            #encrypt-file-folder
+            encrypt_func()
 
-    else:
-        print(" ")
-        #decrypt-file-folder
-        decrypt_func()
-
+        else:
+            print(" ")
+            #decrypt-file-folder
+            decrypt_func()
+    except:
+        print(f"{Back.BLACK}{Fore.RED}[ERROR] pls restart the programm!")
 main()
